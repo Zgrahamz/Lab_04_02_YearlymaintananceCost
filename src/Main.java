6cb2704b8172d05d73464816b84bb7fdbfb780b5
ZGrahamz <graham_zach@outlook.com>
2024-06-03 20:54:53 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        double fallVal = 59.1;
+        double wintVal = 138.2;
+        double sprVal = 65.8;
+        double suVal = 71.5;
+        double totVal = fallVal + wintVal + sprVal + suVal;
+
+        System.out.println("The total home maintenance costs for the year is " + totVal + ", " + "derived from these values from Fall, Winter, Spring, and Summer respectively: " + fallVal + " " + wintVal + " " + sprVal + " " + suVal + ".");
+    }
+}
